ba95cdc560f793a2a84ed73bd6df537525bfef8d
matteoraffetto <91629988+matteoraffetto@users.noreply.github.com>
2021-12-11 12:48:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
-
-int main(){
-  return 0;
+#include "lib.h"
+using namespace std;
+int main() {
+int dim;
+cin>>dim;
+float numeri[dim];
+for(int i=0;i<dim;i++){
+    cin>>numeri[i];
+}
+numeroMassimo(numeri,dim);
+cout << numeroMassimo(numeri,dim) << endl;
+return 0;
 }
