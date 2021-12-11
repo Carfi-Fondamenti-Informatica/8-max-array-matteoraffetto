564fe159da6ad900957338a83952f63bee56081a
matteoraffetto <91629988+matteoraffetto@users.noreply.github.com>
2021-12-11 12:49:25 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,10 @@
+//
+// Created by matte on 30/11/2021.
+//
+
+#ifndef ESERCITAZIONE8_LIB_H
+#define ESERCITAZIONE8_LIB_H
+float numeroMassimo( const float numeri[], int dimensione);
+
+#endif //ESERCITAZIONE8_LIB_H
 
